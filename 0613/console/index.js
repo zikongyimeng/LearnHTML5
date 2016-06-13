ec07e1fced5c6b358f98d13077cef6749f9184e3
zikongyimeng <185464305@qq.com>
2016-06-13 12:05:10 -0700

    g

---
+++ 0613/console/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by sks on 2016/6/13.
+ */
+
+
+window.console.assert("assert");//断言，几乎不用
+window.console.error("error");
+window.console.info("info");
+window.console.log("log");//级别最低
+window.console.warm("warm");
