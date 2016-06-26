770c3904b7f05a32fd0890049e758411796715a8
zikongyimeng <185464305@qq.com>
2016-06-25 20:05:33 -0700

    解析ini文件

---
+++ 0624/home work/ParseIni/app.js
@@ -0,0 +1,19 @@
+/**
+ * Created by plter on 6/25/16.
+ */
+
+
+(function () {
+
+    function init() {
+
+        $.get("config.ini").done(function (data) {
+            var ini = new ucai.Ini(data);
+            
+//          console.log(ini.app.name);
+            console.log(ini.config.window);
+        });
+    }
+
+    init();
+})();
